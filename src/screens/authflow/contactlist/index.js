2695941265770cf977dde70647cd9eba5e103b79
ahmad96263 <ahmadprince248@gmail.com>
2021-07-17 11:34:55 +0500

    new fix

--- src/screens/authflow/contactlist/index.js
+++ src/screens/authflow/contactlist/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import {View,Text, Button} from 'react-native'
 
 
-function Home (){
-
+function Home (props){
+    const { navigate } = props.navigation
     return(
-        <View style={{flex:1,backgroundColor: 'grey'}}>
-            <Text style={{textAlign:'center'}}>ahmad</Text>
-
-         
-        </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Contactlist Screen</Text>
+        <Button
+        title="Go to home"
+        onPress={() => navigate('home')}
+      />
+      </View>
         
         
     )
